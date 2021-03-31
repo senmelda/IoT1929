d85ce8b1ff3ffbe345f64df82b34167bf3314340
Melda Şen <74591461+senmelda@users.noreply.github.com>
2021-03-31 19:42:32 +0300

    1.Hafta3.ProblemSet

---
+++ 1.Hafta3.Set.ino
@@ -0,0 +1,27 @@
+int led = 4;
+void setup()
+{
+  pinMode(A0,INPUT);
+  pinMode(3,OUTPUT);
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int potDeger = analogRead(A0);
+  int yeniDeger = map(potDeger,0,1023,0,255);
+  if (yeniDeger>=100)
+  {
+    digitalWrite( led, HIGH);
+    delay(1000);
+  }
+  else {
+        digitalWrite( led, LOW);
+        delay(1000);
+
+
+  }
+  Serial.println(yeniDeger);
+  delay(10); 
+}
